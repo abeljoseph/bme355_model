d0db4c8b12541b98ffe36de35afecc30109f2ac2
Abel <abeljohnjoseph@gmail.com>
2020-03-30 23:41:55 -0400

    Interpolate shank angle

--- interpolate_data.py
+++ interpolate_data.py
@@ -1,19 +1,18 @@
-from scipy import interpolate
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename = 'x_acc' + '_true.csv'
-degree = 5
+filename = 'shank_angle' + '_true.csv'
+degree = 15
 
 data = []
 with open('data_files/'+filename) as f:
     for line in f:
         data.append([float(x) for x in list(str(line).strip().split(','))])
 
-x = [i[0] for i in data]
+x = [i[0]-data[0][0] for i in data]
 y = [i[1] for i in data]
 
-xnew = np.linspace(0, 0.35, 351)
+xnew = np.linspace(0, data[-1][0]-data[0][0], 351)
 eq = np.polyfit(x, y, degree)
 ynew = 0
 for i,j in zip(range(len(eq)-1, -1, -1), range(0, len(eq))):
