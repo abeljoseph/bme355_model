5a5445fa47eaa427802575d5299a1af6edff8e49
furqansyed <37023276+furqansyed@users.noreply.github.com>
2020-03-30 20:45:49 -0400

    Interpolated data.

---
+++ interpolate_data.py
@@ -0,0 +1,30 @@
+from scipy import interpolate
+import numpy as np
+import matplotlib.pyplot as plt
+
+filename = 'x_acc' + '_true.csv'
+degree = 5
+
+data = []
+with open('data_files/'+filename) as f:
+    for line in f:
+        data.append([float(x) for x in list(str(line).strip().split(','))])
+
+x = [i[0] for i in data]
+y = [i[1] for i in data]
+
+xnew = np.linspace(0, 0.35, 351)
+eq = np.polyfit(x, y, degree)
+ynew = 0
+for i,j in zip(range(len(eq)-1, -1, -1), range(0, len(eq))):
+    ynew += eq[j] * xnew**i
+
+plt.figure()
+plt.plot(x, y, label='Original Data')
+plt.plot(xnew, ynew, '--', label='Polyfit')
+
+plt.legend()
+plt.show()
+
+# Uncomment when happy with degree and ready to save interpolated data
+np.savetxt(filename[:-9]+'_interpolated.csv', np.stack((xnew, ynew), axis=-1), fmt='%s', delimiter=',')
