e9ffb0138fe7a214168adfd56fc48cb20af707cd
Abel Joseph <abeljohnjoseph@gmail.com>
2020-03-31 23:02:59 -0400

    Write to velocity file, add derivative extension

--- angle.py
+++ angle.py
@@ -1,3 +1,4 @@
+import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,8 +13,18 @@ diff = []
 for i in range(0, len(arr)-1):
     dx = arr[:,0][i+1] - arr[:,0][i]
     dy = arr[:,1][i+1] - arr[:,1][i]
-    diff.append(dy/dx)    
-    
+    diff.append([arr[:,0][i], dy/dx])    
+
+diff.append([0.35, diff[-1][1]])  # Copy last value of list to end
+diff = np.array(diff)
+
 plt.scatter(arr[:,0], arr[:,1])
-plt.scatter(arr[:,0][1:], diff)
+plt.scatter(diff[:,0], diff[:,1])
 plt.show()
+
+f = open('data_files/shank_velocity_interpolated.csv', 'w', newline='')
+with f:
+    writer = csv.writer(f)
+
+    for val in diff:
+        writer.writerow(val)
