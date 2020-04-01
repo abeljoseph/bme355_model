62fc3f0565266176955233b7322c70ec06d8f244
furqansyed <37023276+furqansyed@users.noreply.github.com>
2020-03-31 22:15:45 -0400

    First implementation of Model.simulate with many changes to functions.

--- model.py
+++ model.py
@@ -9,7 +9,7 @@ class Model:
     Model of a shank foot undergoing functional electrical stimulus (FES); a Nonlinear State-Space Model
     """
 
-    def __init__(self):
+    def __init__(self, u_profile=np.zeros(351)):
         # Constants mentioned in simulation plan
         self.T_act = 0.01
         self.T_deact = 0.04
@@ -26,8 +26,7 @@ class Model:
         self.w = 0.56
         self.l_t = 22.3
         self.l_mt0 = 32.1
-        self.l_ce = self.l_mt0 - self.l_t
-        self.l_ce_opt = None  # TODO: Define
+        self.l_ce_opt = self.l_mt0 - self.l_t
         self.l_foot = 0.26 #m
         self.a1 = 2.1
         self.a2 = -0.08
@@ -37,20 +36,40 @@ class Model:
         self.a = [self.a1, self.a2, self.a3, self.a4, self.a5]
         self.g = 9.81
 
-        # TODO: Define these variables
-        self.u = u
+        # External States
+        ax = []
+        with open('data_files/x_acc_interpolated.csv') as f:
+            for line in f:
+                ax.append([float(x) for x in list(str(line).strip().split(','))])
         self.ax = ax
+
+        az = []
+        with open('data_files/z_acc_interpolated.csv') as f:
+            for line in f:
+                az.append([float(x) for x in list(str(line).strip().split(','))])
         self.az = az
+
+        a_shank = []
+        with open('data_files/shank_angle_interpolated.csv') as f:
+            for line in f:
+                a_shank.append([float(x) for x in list(str(line).strip().split(','))])
         self.a_shank = a_shank
+        
+        a_shank1 = []
+        with open('data_files/shank_velocity_interpolated.csv') as f:
+            for line in f:
+                a_shank1.append([float(x) for x in list(str(line).strip().split(','))])
         self.a_shank1 = a_shank1
-        self.x_ext = [self.ax, self.az, self.a_shank, self.a_shank1]
-        self.f_act = f_act
+        
+        self.x_external = [self.ax, self.az, self.a_shank, self.a_shank1]
 
-        # TODO: Determine whether to remove these
-        self.a_f = a_f
-        self.a_f1 = a_f1
-        self.x = [self.f_act, self.a_f, self.a_f1]
+        self.u_profile = u_profile
 
+        self.total_sim_time = 0.35
+
+    def set_u_profile(self, u_profile):
+        self.u_profile = u_profile
+    
     def get_torque_grav(self, x):
         """
         :param x: state variables [activation level; foot's absolute orientation wrt horizontal axis; foot's absolute rotational velocity]
@@ -58,12 +77,12 @@ class Model:
         """
         return -self.m_F*self.c_F*self.g*np.cos(x[1])
 
-    def get_torque_acc(self, x):
+    def get_torque_acc(self, x, x_ext):
         """
         :param x: state variables [activation level; foot's absolute orientation wrt horizontal axis; foot's absolute rotational velocity]
         :return: Torque induced by the movement of the ankle
         """
-        return self.m_F*self.c_F*(self.x_ext[0]*np.sin(x[1]) - self.x_ext[1]*np.cos(x[1]))
+        return self.m_F*self.c_F*(x_ext[0]*np.sin(x[1]) - x_ext[1]*np.cos(x[1]))
 
     def get_torque_ela(self, x):
         """
@@ -72,83 +91,100 @@ class Model:
         """
         return np.exp(self.a1 + self.a2 * x[1]) - np.exp(self.a3 + self.a4 * x[1]) + self.a5
 
-    def get_force_fl(self, x):
+    def get_force_fl(self, x, x_ext):
         """
         :param x: state variables [activation level; foot's absolute orientation wrt horizontal axis; foot's absolute rotational velocity]
         :return:
         """
-        w, l_ce, l_ce_opt = self.w, self.l_ce, self.l_ce_opt
+        w, l_ce, l_ce_opt = self.w, self.get_length_ce(x, x_ext), self.l_ce_opt
 
         # TODO: Remove error handling after l_ce_opt is implemented
-        try: return exp(-(-(l_ce - l_ce_opt)/w * l_ce_opt)**2) / (self.x_ext[2] - x[1])
+        try: return exp(-(-(l_ce - l_ce_opt)/w * l_ce_opt)**2) / (x_ext[2] - x[1])
         except TypeError: print("Ensure that model.l_ce_opt is defined.")
     
-    def get_force_fv(self, x):
+    def get_force_fv(self, x, x_ext):
         """
         :param x: state variables [activation level; foot's absolute orientation wrt horizontal axis; foot's absolute rotational velocity]
         :return:
         """
-        v_ce = self.d * (self.x_ext[3] - x[3])  # contraction speed
+        v_ce = self.d * (x_ext[3] - x[3])  # contraction speed
         v_max, a_v, f_v1, f_v2 = self.v_max, self.a_v, self.f_v1, self.f_v2
         if v_ce < 0:
-            return ((1 - v_ce/v_max) / (1 + v_ce/(v_max*f_v1))) / (self.x_ext[3] - x[2])
-        return ((1 + a_v*(v_ce/f_v2)) / (1 + v_ce/f_v2)) / (self.x_ext[3] - x[2])
+            return ((1 - v_ce/v_max) / (1 + v_ce/(v_max*f_v1))) / (x_ext[3] - x[2])
+        return ((1 + a_v*(v_ce/f_v2)) / (1 + v_ce/f_v2)) / (x_ext[3] - x[2])
 
-    def get_force_m(self, x):
-        f_fl, f_fv = self.get_force_fl(x), self.get_force_fv(x)
-        return x[0] * self.f_max * f_fl * (self.x_ext[2] - x[1]) * f_fv * (self.x_ext[3] - x[2])
+    def get_force_m(self, x, x_ext):
+        f_fl, f_fv = self.get_force_fl(x, x_ext), self.get_force_fv(x, x_ext)
+        return x[0] * self.f_max * f_fl * (x_ext[2] - x[1]) * f_fv * (x_ext[3] - x[2])
 
-    def get_length_mt(self, x):
+    def get_length_mt(self, x, x_ext):
         """
         :param x: state variables [activation level; foot's absolute orientation wrt horizontal axis; foot's absolute rotational velocity]
         :return:
         """
-        return self.l_mt0 + (self.x_ext[2] - x[1])*self.d
+        return self.l_mt0 + (x_ext[2] - x[1])*self.d
+
+    def get_length_ce(self, x, x_ext):
+        """
+        :param x: state variables [activation level; foot's absolute orientation wrt horizontal axis; foot's absolute rotational velocity]
+        :return:
+        """
+        return self.get_length_mt(x, x_ext) - self.l_t
 
     def get_toe_height(self, ankle_height, ankle_angle):
         return ankle_height - self.l_foot*np.sin(ankle_angle)
 
-    def get_derivative(self, t, x, u):
+    def get_derivative(self, t, x):
         """
         :param t: time
         :param x: state variables [activation level; foot's absolute orientation wrt horizontal axis; foot's absolute rotational velocity]
         :return: time derivatives of state variables
         """
+        u = self.u_profile[int(t*1000)]
+        x_ext = [i[int(t*1000)][1] for i in self.x_external]
+
         return [(u - x[0]) * (u/self.T_act - (1 - u)/self.T_deact),
                 x[2],
-                (1/self.J) * self.get_force_m(x[0])*self.d + self.get_torque_grav(x) + self.get_torque_acc(x) + self.get_torque_ela(x)]
+                (1/self.J) * self.get_force_m(x, x_ext)*self.d + self.get_torque_grav(x) + self.get_torque_acc(x, x_ext) + self.get_torque_ela(x)]
 
-    def simulate(self, total_time):
+    def simulate(self):
         """
         :param total_time: seconds to simulate
         :return: time, state (times at which the state is estimated, state vector at each time)
         """
-        # Decide an initial condition, this might change due to all the variables present.
-        x0 = [0, 0, 0]
-        t_span = (0, total_time)
-        dt = 0.01
+        # Initial State based off paper
+        f_act = 0
+        a_f = 0
+        a_f1 = -15
+        y0 = [f_act, a_f, a_f1]
 
-        sol = solve_ivp(self.get_derivative, t_span, x0, max_step=dt)
+        t_span = (0, self.total_sim_time)
+        dt = 0.001
+
+        sol = solve_ivp(self.get_derivative, t_span, y0, max_step=dt)
         return sol.t, sol.y.T
 
 
 def plot_graphs(model, time, states):
-    # plt.title('States of Circulation versus Time')
-    # plt.plot(time, states[:, 0], 'k', label='Ventricular Pressure')
-    # plt.plot(time, states[:, 1], 'g', label='Atrial Pressure')
-    # plt.plot(time, states[:, 2], 'r', label='Arterial Pressure')
-    # plt.plot(time, aortic_pressure, 'c', label='Aortic Pressure')
-    # plt.ylabel('Pressure (mmHg)')
-    # plt.xlabel('Time (s)')
-    # plt.legend(loc='upper left')
-    # plt.show()
-    pass
+    plt.figure()
+    plt.title('States of Foot versus Time')
+    plt.subplot(2, 1, 1)
+    plt.plot(time, states[:, 0], 'k')
+    plt.ylabel('Activation Level')
+    plt.subplot(2, 1, 2)
+    plt.plot(time, states[:, 1], 'g')
+    plt.ylabel('Absolute Orientation wrt Horizontal Axis (deg)')
+    plt.subplot(2, 1, 3)
+    plt.plot(time, states[:, 2], 'r')
+    plt.xlabel('Time(s)')
+    plt.ylabel('Absolute Rotational Velocity (deg/s)')
+    plt.tight_layout()
+    plt.show()
 
 
 if __name__ == '__main__':
     # Initiate Model
-    model = Model()
-    
+    model = Model()    
     # Simulate Model
-    t, x = model.simulate(10)
+    t, x = model.simulate()
     plot_graphs(model, t, x)
